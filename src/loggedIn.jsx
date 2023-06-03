27303ce7d49b9adc73b1829b9577b810f3ec8f74
@BT-code <mtbalraj@gmail.com>
2023-06-03 09:45:05 +0530

    solved: changes

--- src/loggedIn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const loggedIn = () => {
-  return (
-    <div className='bg-gradient-to-b from-gray-900 to-gray-600'>Hello User, You Logged In Successfully</div>
-  )
-}
-
-export default loggedIn
